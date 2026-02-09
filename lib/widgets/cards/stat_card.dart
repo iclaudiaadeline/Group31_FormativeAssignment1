c746003c929515c4193e6ab087b65e5bb804a43e
iclaudiaadeline <a.iradukund3@alustudent.com>
2026-02-09 23:06:53 +0200

    update

---
+++ lib/widgets/cards/stat_card.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../../core/theme/app_colors.dart';
+
+class StatCard extends StatelessWidget {
+  final String value;
+  final String label;
+
+  const StatCard({super.key, required this.value, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: AppColors.card,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          children: [
+            Text(value,
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold)),
+            const SizedBox(height: 6),
+            Text(label, style: const TextStyle(color: Colors.white70)),
+          ],
+        ),
+      ),
+    );
+  }
+}
